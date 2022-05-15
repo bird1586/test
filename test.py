40ddae31b4b740f75d3eaabd5aeacdd1cebde723
Chingyen <42596154+bird1586@users.noreply.github.com>
2022-05-15 17:04:19 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 15 17:03:10 2022
+
+@author: bird1586
+"""
+
+from docxtpl import DocxTemplate
+import streamlit as st
+
+doc = DocxTemplate("Ctrip舉牌資訊.docx")
+st.write("Yes")
